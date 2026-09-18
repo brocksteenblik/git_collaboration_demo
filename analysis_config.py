8826aad831ae39dde3931a4cf46a9aea51792002
Jeremy Ng <168603096+jn552@users.noreply.github.com>
2026-09-18 10:46:36 -0600

    added stdev and min

--- analysis_config.py
+++ analysis_config.py
@@ -1,8 +1,5 @@
 """Configuration used in the merge conflict demonstration."""
 
-<<<<<<< HEAD
-SUMMARY_METRICS = ["mean", "median", "standard_deviation"]
-=======
-SUMMARY_METRICS = ["mean", "median", "minimum"]
->>>>>>> main
+SUMMARY_METRICS = ["mean", "median", "standard_deviation", "minimum"]
+
 
